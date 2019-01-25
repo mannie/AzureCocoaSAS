d5b0b98001c6b0402ccf1a95ad5a05dd99567a81
Mannie Tagarira <manniet@microsoft.com>
2019-01-25 10:55:55 -0500

    Initial Commit

---
+++ AzureCocoaSAS/AzureCocoaSAS.h
@@ -0,0 +1,19 @@
+//
+//  AzureCocoaSAS.h
+//  AzureCocoaSAS
+//
+//  Created by Mannie Tagarira on 1/25/19.
+//  Copyright © 2019 Mannie Tagarira. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AzureCocoaSAS.
+FOUNDATION_EXPORT double AzureCocoaSASVersionNumber;
+
+//! Project version string for AzureCocoaSAS.
+FOUNDATION_EXPORT const unsigned char AzureCocoaSASVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AzureCocoaSAS/PublicHeader.h>
+
+
